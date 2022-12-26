51b960ddb6a43bccbe2c99633713f430f2280c1c
albertogarel <qmelias@gmail.com>
2022-12-26 15:21:14 +0100

    Added .env file and configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = (api) => {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo', ['@babel/preset-env', {targets: {node: 'current'}}]],
+        presets: [
+            'babel-preset-expo',
+            ['@babel/preset-env', {targets: {node: 'current'}}]
+        ],
+        plugins: [
+            ['module:react-native-dotenv', {
+                "envName": "APP_ENV",
+                "moduleName": "@env",
+                "path": ".env",
+                "blocklist": null,
+                "allowlist": null,
+                "safe": false,
+                "allowUndefined": true,
+                "verbose": false
+            }]
+        ]
     };
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+// @ts-ignore
+import {APP_NAME} from '@env'
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!{APP_NAME}</Text>
             <StatusBar/>
         </View>
     );
